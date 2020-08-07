7c54c3844522e686cfb956e272515e2212faf9a5
Yazan Khatib <yazankhatib97@gmail.com>
2020-08-07 12:40:53 +0300

    add: Initial file modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Main from './pages/main';
 
 function App() {
-  return <div className="App"></div>;
+  return <Main />;
 }
 
 export default App;
